2d59f5a160200f1a5b6f65b4a61e2d09236a5994
master_dz_developer <js_chahbouni@esi.dz>
2022-12-23 19:09:01 +0100

    first_commit

---
+++ app/Http/Requests/ProfileUpdateRequest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Models\Freelancer;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class ProfileUpdateRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'name' => ['string', 'max:255'],
+            'email' => ['email', 'max:255', Rule::unique(Freelancer::class)->ignore($this->user()->id)],
+        ];
+    }
+}
